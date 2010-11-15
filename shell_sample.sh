f971a4c3cb5baa88d57c7e5290c2182205d612a6
Matthieu Poullet <m.poullet@avm.de>
2010-11-15 16:54:17 +0100

    Use Linux Kernel formatting style

--- shell_sample.sh
+++ shell_sample.sh
@@ -3,24 +3,24 @@
 VERSION=1.0
 
 goto_script_home() {
-export SCRIPT_HOME=$(readlink -f $(dirname $0))
+SCRIPT_HOME=$(readlink -f $(dirname $0))
 cd ${SCRIPT_HOME}
 }
 
 parse_command_line() {
 while getopts ":hv-:" option ; do
-if [ "${option}" = "-" ]; then
-case ${OPTARG} in
-help ) option=h ;;
-version ) option=v ;;
-	* ) echo "Unknown option ${OPTARG}" ;;
-esac
-fi
-case $option in
-h ) print_usage ;;
-v ) echo "${SCRIPT_NAME} ${VERSION}" ;;
-? ) echo "Unknown option" ;;
-esac
+	if [ "${option}" = "-" ]; then
+		case ${OPTARG} in
+		help ) option=h ;;
+		version ) option=v ;;
+		* ) echo "Unknown option ${OPTARG}" ;;
+		esac
+	fi
+	case $option in
+		h ) print_usage ;;
+		v ) echo "${SCRIPT_NAME} ${VERSION}" ;;
+		? ) echo "Unknown option" ;;
+	esac
 done
 
 shift $((OPTIND - 1))
